d47348ab291f5d797b4be26c9d31b1cb949dd884
Gabriel Nakhata <gabrielnakata@gmail.com>
2022-02-01 23:39:01 -0300

    subindo código para repositório...

---
+++ my-first-web-api/Controllers/Util/DAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace my_first_web_api.Controllers.Util
+{
+    public class DAL
+    {
+        private static string Server = "localhost";
+        private static string Database = "dbcliente";
+        private static string User = "root";
+        private static string Password = "*705FAD174FACA7A7099A435F17C5D08537A92CDE";
+        private MySqlConnection Connection;
+
+        private string ConnectionString = $"Server={Server};Database={Database};Uid={User};Pwd={Password};Sslmode=none;";
+
+        public DAL() {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+        }
+
+        //Executa: INSERT, UPDATE, DELETE
+        public void ExecutarComandoSQL(string sql)
+        {
+            MySqlCommand Command = new MySqlCommand(sql, Connection);
+            Command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ my-first-web-api/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using my_first_web_api.Controllers.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_first_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            DAL objDAL = new DAL();
+
+            string sql = "insert into cliente(nome, data_cadastro, cpf_cnpj, data_nascimento, tipo, telefone, email, cep, logradouro, numero, bairro, complemento, cidade, uf)" +
+                         "values('Gabriel', '2022/02/28', '09609089766', '1990/09/14', 'F', '998784512', 'gabrielnakata@gmail.com', '31635110', 'Rua joaquim de Figueiredo', '400', 'Barreiro', 'loja', 'Belo Horizonte', 'Minas Gerais')";
+
+            objDAL.ExecutarComandoSQL(sql);
+
+            return new string[] { "value1", "value2" };
+
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+
+        public void Post([FromBody]string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+
+        public void Put(int id, [FromBody]string value)
+        { 
+        }
+    }
+}
